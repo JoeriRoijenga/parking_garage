fb5b5872f0f52ddfb4e2f32c0ee5e42ee3354077
Timo de Jong <tjdejong98@gmail.com>
2019-01-17 13:39:43 +0100

    added comments to QueueVehicle and moves queue attribute from queueCar to queueVehicle

--- src/nl/hanze/st/parkeersimulator/model/QueueCar.java
+++ src/nl/hanze/st/parkeersimulator/model/QueueCar.java
@@ -4,8 +4,6 @@ import java.util.*;
 
 public class QueueCar extends QueueVehicle {
 
-	private Queue<Car> queue = new LinkedList<>();
-
 	public QueueCar() {
 	}
 
--- src/nl/hanze/st/parkeersimulator/model/QueueVehicle.java
+++ src/nl/hanze/st/parkeersimulator/model/QueueVehicle.java
@@ -2,16 +2,44 @@ package nl.hanze.st.parkeersimulator.model;
 
 import java.util.*;
 
-public abstract class QueueVehicle {
+/**
+ * Class QueueVehicle
+ * 
+ * This class is made for editing and creating a queue, in which Vehicles are.
+ * And can be used to get information from the queue.
+ * 
+ * @author Timo de Jong, Joeri Roijenga, Tim Perdok, Niels. 
+ * @version 0.1 (16-1-2019)
+ */
 
+public abstract class QueueVehicle {
+	
+	private Queue<Car> queue = new LinkedList<>();
+
+	/**
+	 * Adds a Vehicle to the queue.
+	 * 
+	 * @param car
+	 * @return boolean
+	 */
     public boolean addVehicle(Car car) {
         return queue.add(car);
     }
 
+    /**
+     * Removes a Vehicle from the queue
+     * 
+     * @return Car
+     */
     public Car removeVehicle() {
         return queue.poll();
     }
 
+    /**
+     * Return the size of the queue
+     * 
+     * @return integer
+     */
     public int vehiclesInQueue(){
     	return queue.size();
     }
