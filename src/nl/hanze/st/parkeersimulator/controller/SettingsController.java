142e867e7361addc213dce77f36282bece1f1b8e
joeriroijenga <roijenga.joeri@gmail.com>
2019-02-01 11:27:38 +0100

    error msg

--- src/nl/hanze/st/parkeersimulator/controller/SettingsController.java
+++ src/nl/hanze/st/parkeersimulator/controller/SettingsController.java
@@ -1,5 +1,10 @@
 package nl.hanze.st.parkeersimulator.controller;
 
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.*;
 
 import nl.hanze.st.mvc.Controller;
@@ -52,33 +57,52 @@ public class SettingsController extends Controller {
 	protected boolean event(View view, int event_id) {
 		switch (event_id) {
 			case EVENT_ID_SAVE:
-				settingsView = ((MenuSettingsView) view).settingsFrame;
-				JPanel settings = (JPanel) ((JPanel) settingsView.getContentPane().getComponent(0)).getComponent(0);
-				
-				JTextField exitSpeed = (JTextField) settings.getComponent(1);
-				JTextField paymentSpeed = (JTextField) settings.getComponent(3);
-				
-				JTextField weekDayArrivals = (JTextField) settings.getComponent(5);
-				JTextField weekendArrivals = (JTextField) settings.getComponent(7);
-				
-				JTextField weekDayPassArrivals = (JTextField) settings.getComponent(9);
-				JTextField weekendPassArrivals = (JTextField) settings.getComponent(11);
-				
-				JTextField reservationChance = (JTextField) settings.getComponent(13);
-				
-				garage.setExitSpeed(Integer.parseInt(exitSpeed.getText()));
-				garage.setPaymentSpeed(Integer.parseInt(paymentSpeed.getText()));
-				
-				garage.setWeekDayArrivals(Integer.parseInt(weekDayArrivals.getText()));
-				garage.setWeekendArrivals(Integer.parseInt(weekendArrivals.getText()));
-				
-				garage.setWeekDayPassArrivals(Integer.parseInt(weekDayPassArrivals.getText()));
-				garage.setWeekendPassArrivals(Integer.parseInt(weekendPassArrivals.getText()));
-				
-				garage.setReservationChance(Integer.parseInt(reservationChance.getText()));				
-				
-				settingsView.dispose();
-				return true;
+				try {
+					settingsView = ((MenuSettingsView) view).settingsFrame;
+					JPanel settings = (JPanel) ((JPanel) settingsView.getContentPane().getComponent(0)).getComponent(0);
+					
+					JTextField exitSpeed = (JTextField) settings.getComponent(1);
+					JTextField paymentSpeed = (JTextField) settings.getComponent(3);
+					
+					JTextField weekDayArrivals = (JTextField) settings.getComponent(5);
+					JTextField weekendArrivals = (JTextField) settings.getComponent(7);
+					
+					JTextField weekDayPassArrivals = (JTextField) settings.getComponent(9);
+					JTextField weekendPassArrivals = (JTextField) settings.getComponent(11);
+					
+					JTextField reservationChance = (JTextField) settings.getComponent(13);
+					
+					garage.setExitSpeed(Integer.parseInt(exitSpeed.getText()));
+					garage.setPaymentSpeed(Integer.parseInt(paymentSpeed.getText()));
+					
+					garage.setWeekDayArrivals(Integer.parseInt(weekDayArrivals.getText()));
+					garage.setWeekendArrivals(Integer.parseInt(weekendArrivals.getText()));
+					
+					garage.setWeekDayPassArrivals(Integer.parseInt(weekDayPassArrivals.getText()));
+					garage.setWeekendPassArrivals(Integer.parseInt(weekendPassArrivals.getText()));
+					
+					garage.setReservationChance(Integer.parseInt(reservationChance.getText()));				
+					
+					settingsView.dispose();
+					return true;
+				} catch(Exception e) {
+					JFrame exceptionFrame = new JFrame("Failed");
+					exceptionFrame.setSize(500,200);
+					exceptionFrame.setLayout(new GridLayout(3,3));
+					
+					JLabel textLabel = new JLabel("Save Failed!",JLabel.CENTER);
+					textLabel.setFont(new Font("Sans", Font.PLAIN, 20));
+					JLabel errorLabel = new JLabel("One of the values is invalid.",JLabel.CENTER);
+					exceptionFrame.addWindowListener(new WindowAdapter() {
+						public void windowClosing(WindowEvent windowsEvent) {
+							exceptionFrame.dispose();
+						}
+					});		
+					
+					exceptionFrame.add(textLabel);
+					exceptionFrame.add(errorLabel);					
+					exceptionFrame.setVisible(true);
+				}
 			default:
 				return false;
 		}
